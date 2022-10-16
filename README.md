e5b90a4c409abc6ccef4800912f63b3c34bebea6
Yohanson555 <yohanson555@gmail.com>
2022-10-16 14:05:44 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# 3d-perspective-gallery
+### Overview
+
+Created with course: https://www.youtube.com/watch?v=GLbI7BGdQ3o&t=57s
+
+
+### What's interesting
+
+- CSS: `perspective`
+- CSS: content scaling wioth size index (--index var)
+- CSS: `width: min-content;`
+- CSS: relative sizes (--side-small & --side-big)
+- CSS: working with audio
+- CSS: `will-change: transform;` property
+
+- JS: audio actions. 
+- JS: `window.onfocus` & `window.onblur` events
+- JS: using of `audioBtn.classList`
---
+++ js/app.js
@@ -0,0 +1,50 @@
+// 3d scrool
+
+const zSpacing = -1000;
+let lastPos = zSpacing / 5;
+
+const $frames = document.getElementsByClassName('frame');
+const framesArray = Array.from($frames);
+
+const zVals = [];
+
+framesArray.forEach((n, i) => {
+    zVals.push((i * zSpacing) + zSpacing);
+});
+
+// refactor with lyambda
+window.onscroll = function () {
+    let top = document.documentElement.scrollTop;
+    let delta = lastPos - top;
+
+    lastPos = top;
+
+    framesArray.forEach((n, i) => {
+        zVals[i] += delta * -3;
+
+        const frame = framesArray[i];
+        const transform = `translateZ(${zVals[i]}px)`;
+        const opacity = zVals[i] < Math.abs(zSpacing / 1.8) ? 1 : 0;
+
+        frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity};`);
+    });
+};
+
+// audio
+
+const audioBtn = document.getElementById('audioBtn');
+const audio = document.getElementById('audio')
+
+audioBtn.onclick = function (e) {
+    audioBtn.classList.toggle('paused');
+    audio.paused ? audio.play() : audio.pause();
+
+};
+
+window.onfocus = function () {
+    audioBtn.classList.contains('paused') ? audio.pause() : audio.play();
+};
+
+window.onblur = function () {
+    audio.pause();
+};
